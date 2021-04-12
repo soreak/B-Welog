44c5ff1c2b611688de400c582a566a739295b70b
soreak <1498985670@qq.com>
2021-04-12 20:41:02 +0800

    用户端  登录后忘记密码部分完成 本系统告一段落

--- src/main/java/com/soreak/controller/UserController.java
+++ src/main/java/com/soreak/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.soreak.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.soreak.entity.UserEntity;
 import com.soreak.entity.VO.BlogVO;
 import com.soreak.service.BlogLikeService;
@@ -11,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -40,6 +43,9 @@ public class UserController {
     @Autowired
     private BlogService blogService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
 
     @Value("${absoluteImgPath}")
@@ -47,6 +53,7 @@ public class UserController {
 
     @Value("${sonImgPath}")
     String sonImgPath;
+
 
     @GetMapping("/showInfo/{id}")
     @PreAuthorize("isAuthenticated()")
@@ -152,7 +159,43 @@ public class UserController {
 
     }
 
+    @RequestMapping(value = "/editPassword",method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    public String editPassword(Model model){
+        getUser(model);
+        return "resetPwd";
+    }
 
+
+
+    @PostMapping("/loginResetPwd")
+    @ResponseBody
+    @PreAuthorize("isAuthenticated()")
+    public JSONObject loginResetPwd(@RequestParam("oldPwd") String oldPwd,
+                                    @RequestParam("newPwd") String newPwd,
+                                    HttpSession session){
+        String phone = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = userService.findByPhone(phone);
+       JSONObject jsonObject = new JSONObject();
+       if (!passwordEncoder.matches(oldPwd,user.getPassword())){
+           jsonObject.put("status",500);
+           jsonObject.put("message","原密码错误");
+           return jsonObject;
+       }
+
+        if (oldPwd.equals(newPwd)){
+            jsonObject.put("status",500);
+            jsonObject.put("message","不能设置与之前相同的密码");
+            return jsonObject;
+        }
+        String newP = passwordEncoder.encode(newPwd);
+        user.setPassword(newP);
+        userService.saveUser(user);
+        jsonObject.put("status",200);
+        jsonObject.put("message","修改成功，请重新登录");
+        session.removeAttribute("user");
+        return jsonObject;
+    }
 
 
 
